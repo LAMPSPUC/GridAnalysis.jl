90c3f065f9090fee09a109607476622b128f8607
Documenter.jl <documenter@juliadocs.github.io>
2021-05-04 13:09:41 +0000

    build based on 2da0662

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GridAnalysis","category":"page"},{"location":"#GridAnalysis","page":"Home","title":"GridAnalysis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GridAnalysis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GridAnalysis]","category":"page"}]
+}
