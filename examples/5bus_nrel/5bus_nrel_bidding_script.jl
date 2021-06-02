03c5afdf55b3d897134d9365c11a57043f7a4ab9
DanielledeMacedo <danielle.macedo@engenharia.ufjf.br>
2021-06-02 13:44:44 -0300

    updating

--- examples/5bus_nrel/5bus_nrel_bidding_script.jl
+++ examples/5bus_nrel/5bus_nrel_bidding_script.jl
@@ -37,7 +37,6 @@ base_system = build_5_bus_matpower_DA(
 
 # Add single generator at a defined bus
 node = "bus5" # define bus
-active_power_limits = (min=0.0, max=0.0) # define maximum bid for generator
 gen = add_gerator!(base_system, node, active_power_limits)
 
 # create and set variable cost time-series for the generator
@@ -45,86 +44,32 @@ ts_array = create_generator_bids(; initial_bidding_time=DateTime("2020-01-01"), 
 set_variable_cost!(base_system, gen, ts_array)
 
 #Define range quota
-range_quota=collect(0:0.1:8)
-
-# duplicate system and prepare times series for the time varying parameters (loads, renewables, ...)
-sys_uc, sys_ed = prep_systems_UCED(base_system)
-
-# generic market formulation templates with defined network formulation
-# CopperPlate-OPF: network=CopperPlatePowerModel
-# DC-OPF: network=DCPPowerModel
-# NFA-OPF (only line limit constraints): network=NFAPowerModel
-# DC-PTDF-OPF (what ISOs do): network=StandardPTDFModel
-template_uc = template_unit_commitment(; network=DCPPowerModel)
-template_ed = template_economic_dispatch(; network=DCPPowerModel)
-
-# build a market clearing simulator (run `@doc UCED` for more information)
-market_simulator = UCED(;
-    system_uc=sys_uc,
-    system_ed=sys_ed,
-    template_uc=template_uc,
-    template_ed=template_ed,
-    solver_uc=solver_uc,
-    solver_ed=solver_ed,
-)
-
-@test isa(market_simulator, UCED)
+range_quota=collect(0:1:5)
 
 # Virtual Bids Simulation 
-initial_time = DateTime("2020-01-01")
+initial_time = Date("2020-01-01")
 lmps_df, results_df = pq_curves_da(
-    base_system;
-    gen,
+    gen;
+    base_system,
     range_quota,
     initial_time, #: TODO: The same as ts_array
-    market_simulator,
-    steps = 1,
-    simulation_folder = joinpath(example_dir, "results")
+    steps=1,
+    simulation_folder = joinpath(example_dir, "results"),
+    solver_uc,
+    solver_ed
 ) #:TODO: Plots
 
-#=
 
-# for each formulation you will need to save different dual variables:
-constraint_duals = duals_constraint_names(market_simulator)
-
-@test isa(constraint_duals, AbstractVector{Symbol})
-
-# Simulate market
-# build and run simulation
-results = run_multiday_simulation(
-    market_simulator,
-    Date("2020-01-01"), # initial time for simulation
-    1; # number of steps in simulation (normally number of days to simulate)
-    services_slack_variables=false,
-    balance_slack_variables=false,
-    constraint_duals=constraint_duals,
-    name="test_case_5bus",
-    simulation_folder=mktempdir(), # Locally can use: joinpath(example_dir, "results"),
-);
-
-@test isa(results, SimulationResults)
-
-# separate results
-uc_results = get_problem_results(results, "UC");
-ed_results = get_problem_results(results, "ED");
-
-# calculate prices
-prices = evaluate_prices(market_simulator, ed_results)
-
-@test isa(prices, DataFrame)
-
-# virtual generation
-get_fuel(gen)
-
-p_5=prices[!,:"bus5"]
-
-variable_results = read_realized_variables(uc_results, names=[:P__ThermalStandard])
+max_gen=4
+bus=get_name(get_bus(gen))
+variable_results = read_realized_variables(results_df[max_gen], names=[:P__ThermalStandard])
 generator_data = getindex.(Ref(variable_results), [:P__ThermalStandard])
+lmps=lmps_df[max_gen]
+virtual_gen=generator_data[1][!,:7]
+p=lmps[!,:"bus5"] # TODO: Put bus here
+revenue=p.*virtual_gen
 
-vitual_gen=generator_data[1][!,:7]
-revenue=p_5.*virtual_gen
-=#
-
+#=
 # Plots
 plot_generation_stack(base_system, ed_results; xtickfontsize=8, margin=8mm, size=(800, 600))
 plot_generation_stack(
@@ -190,3 +135,4 @@ plot_net_demand_stack(sys_uc, uc_results; xtickfontsize=8, size=(800, 600))
 plot_net_demand_stack(
     sys_uc, uc_results; bus_names=["bus2", "bus3"], xtickfontsize=8, size=(800, 600)
 )
+=#
--- src/gen_curves.jl
+++ src/gen_curves.jl
@@ -1,18 +1,46 @@
+"""
+    pq_curves_da(generator::ThermalStandard; base_system::System,range_quota::Vector{Int64}, initial_time::Date, steps::Int = 1, simulation_folder::String = pwd(), solver_uc::MathOptInterface.OptimizerWithAttributes, solver_ed::MathOptInterface.OptimizerWithAttributes)
+
+Creates a curve of generation and nodes prices for a vector of 'range_quotas' for max generation of a virtual 'generator' in a 'base_system'. 
+"""
 function pq_curves_da(
-    sys;
-    generator,
-    range_quota,
-    initial_time,
-    market_simulator,
-    steps = 1,
-    simulation_folder = pwd(),
-    kwargs...
+    generator::ThermalStandard;
+    base_system::System,
+    range_quota::Vector{Int64},
+    initial_time::Date,
+    steps::Int = 1,
+    simulation_folder::String = pwd(),
+    solver_uc,
+    solver_ed
 )
     lmps_df = Dict()
     results_df = Dict()
+
     for max_gen in range_quota
         set_active_power_limits!(generator, (min = 0.0, max = max_gen))
+       
+        # duplicate system and prepare times series for the time varying parameters (loads, renewables, ...)
+        sys_uc, sys_ed = prep_systems_UCED(base_system)
 
+        # generic market formulation templates with defined network formulation
+        # CopperPlate-OPF: network=CopperPlatePowerModel
+        # DC-OPF: network=DCPPowerModel
+        # NFA-OPF (only line limit constraints): network=NFAPowerModel
+        # DC-PTDF-OPF (what ISOs do): network=StandardPTDFModel
+        template_uc = template_unit_commitment(; network=DCPPowerModel)
+        template_ed = template_economic_dispatch(; network=DCPPowerModel)
+
+        # build a market clearing simulator (run `@doc UCED` for more information)
+        market_simulator = UCED(;
+            system_uc=sys_uc,
+            system_ed=sys_ed,
+            template_uc=template_uc,
+            template_ed=template_ed,
+            solver_uc=solver_uc,
+            solver_ed=solver_ed,
+        )
+
+        @test isa(market_simulator, UCED)
         # for each formulation you will need to save different dual variables:
         constraint_duals = duals_constraint_names(market_simulator)
 
@@ -30,9 +58,9 @@ function pq_curves_da(
             #=name="test_case_5bus",
             simulation_folder=mktempdir(), # Locally can use: joinpath(example_dir, "results"),
             =#
-            simulation_folder = simulation_folder,
             name = "quota_$max_gen",
-            kwargs...
+            simulation_folder = simulation_folder#,
+            #kwargs...
         );
 
         @test isa(results, SimulationResults)
@@ -47,23 +75,6 @@ function pq_curves_da(
         results_df[max_gen] = get_problem_results(results, "UC");
         lmps_df[max_gen] = prices
 
-        #= virtual generation
-        bus=get_bus(generator)
-        p=prices[!,:bus]
-
-        variable_results = read_realized_variables(uc_results, names=[:P__ThermalStandard])
-        generator_data = getindex.(Ref(variable_results), [:P__ThermalStandard])
-
-        vitual_gen=generator_data[1][!,:7] #?
-        revenue=p.*virtual_gen
-        =#
-
-        #=
-        results_df[max_gen] = get_problem_results(results, "UC")
-        ed_results = get_problem_results(results, "ED")
-        ptdf = PTDF(sys_ed)
-        lmps_df[max_gen] = psi_ptdf_lmps(ed_results, sys_ed, ptdf)
-        =#
     end
     return lmps_df, results_df
 
