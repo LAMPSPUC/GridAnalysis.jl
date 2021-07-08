125740dd9c1c145c57a5a6c0399afbc8ba5aa15a
DanielledeMacedo <danielle.macedo@engenharia.ufjf.br>
2021-07-08 20:36:44 -0300

    create functions to run and load a set of simulat

--- examples/5bus_nrel/utils.jl
+++ examples/5bus_nrel/utils.jl
@@ -213,3 +213,213 @@ function plot_DA_RT(prices)
         palette=palette,
     )
 end
+
+
+"""
+    run_set_of_simulations(
+        df::DataFrame, 
+        data_dir::String, 
+        example_dir::String, 
+        solver_uc, 
+        solver_ed, 
+        solver_rt, 
+        range_quota::Vector{Float64}, 
+        initial_time::Date, 
+        initial_bidding_time::DateTime
+    )
+
+Run a set of simulations to 5bus_nrel with descripted sistems as in 'df'
+"""
+function run_set_of_simulations(df::DataFrame, 
+    data_dir::String, 
+    example_dir::String, 
+    solver_uc, 
+    solver_ed, 
+    solver_rt, 
+    range_quota::Vector{Float64}, 
+    initial_time::Date, 
+    initial_bidding_time::DateTime
+)
+    for l=1:size(df)[1]
+
+        directory_name= "Network_" * string(df.Network[l]) * "__Ramp_" * string(df.Ramp[l]) * 
+        "__Min_gen_" * string(df.Minimal_generation[l]) * "__Reserve_" * string(df.Reserve[l]) *
+        "__Offer_Bus_" * string(df.Offer_Bus[l]) * "__bidding_period_1-" * string(length(df.bidding_period[l]))
+
+        if isdir(joinpath(example_dir, "price_impact_analysis_5bus_nrel/results", directory_name))==false
+
+            # call our data preparation to build base system
+            # the case was modified to not have hydros nor transformers
+            sys_rt = build_5_bus_matpower_RT(data_dir;)
+
+            base_da_system = build_5_bus_matpower_DA(
+                data_dir;
+                # using a modified (mod) file that reduced load for feasibility in DC-OPF
+                forecasts_pointers_file=joinpath(
+                    data_dir, "forecasts", "timeseries_pointers_da_7day_mod.json"
+                ),
+                add_reserves=df.Reserve[l],
+            )
+
+
+            # Add single generator at a defined bus
+            gen = add_gerator!(base_da_system, df.Offer_Bus[l], (min=0.0, max=0.0))
+            @test gen in get_components(Generator, base_da_system)
+
+            # create and set variable cost time-series for the generator
+            ts_array = create_generator_bids(;
+                initial_bidding_time=initial_bidding_time,
+                bidding_periods=df.bidding_period[l],
+                system=base_da_system,
+                costs=zeros(length(df.bidding_period[l])),
+            )
+            set_variable_cost!(base_da_system, gen, ts_array)
+
+            # duplicate system and prepare times series for the time varying parameters (loads, renewables, ...)
+            sys_uc, sys_ed = prep_systems_UCED(base_da_system)
+
+            # generic market formulation templates with defined network formulation
+            # CopperPlate-OPF: network=CopperPlatePowerModel
+            # DC-OPF: network=DCPPowerModel
+            # NFA-OPF (only line limit constraints): network=NFAPowerModel
+            # DC-PTDF-OPF (what ISOs do): network=StandardPTDFModel
+            template_uc = template_unit_commitment(; network=df.Network[l])
+            template_rt = template_economic_dispatch(; network=df.Network[l])
+            template_ed = template_economic_dispatch(; network=df.Network[l])
+
+            # build a market clearing simulator
+            market_simulator = UCEDRT(;
+                system_uc=sys_uc,
+                system_rt=sys_rt,
+                system_ed=sys_ed,
+                template_uc=template_uc,
+                template_rt=template_rt,
+                template_ed=template_ed,
+                solver_uc=solver_uc,
+                solver_rt=solver_rt,
+                solver_ed=solver_ed,
+            )
+
+            @test isa(market_simulator, UCEDRT)
+
+            #Calculates the dispatch result for a bid curve
+            name_generator = get_name(gen);
+            steps = 1;
+
+            mkdir(joinpath(example_dir, "price_impact_analysis_5bus_nrel/results", directory_name))
+            simulation_folder = joinpath(example_dir, "price_impact_analysis_5bus_nrel/results", directory_name)
+
+            lmps_df, results_df = pq_curves_virtuals!(
+                market_simulator, name_generator, range_quota, initial_time, steps, simulation_folder
+            )
+
+        end
+    end
+end
+
+"""
+    load_set_of_simulations(
+        df::DataFrame, 
+        data_dir::String, 
+        example_dir::String, 
+        solver_uc, 
+        solver_ed, 
+        solver_rt, 
+        range_quota::Vector{Float64}, 
+        initial_time::Date, 
+        lines::Vector{Float64},
+        period_analysed::Vector{Vector{Int64}},
+        initial_bidding_time::DateTime
+    )
+
+Load a set of simulations of 5bus_nrel with descripted sistems in 'lines' from 'df'
+"""
+function load_set_of_simulations(
+    df::DataFrame, 
+    data_dir::String, 
+    example_dir::String, 
+    solver_uc, 
+    solver_ed, 
+    solver_rt, 
+    range_quota::Vector{Float64}, 
+    initial_time::Date, 
+    lines::Vector{Float64},
+    period_analysed::Vector{Vector{Int64}},
+    initial_bidding_time::DateTime
+)
+
+    plt=Array{Any}(nothing, length(lines), length(period_analysed))
+
+    for (x,l) in enumerate(lines)
+
+        # call our data preparation to build base system
+        # the case was modified to not have hydros nor transformers
+        sys_rt = build_5_bus_matpower_RT(data_dir;)
+
+        base_da_system = build_5_bus_matpower_DA(
+            data_dir;
+            # using a modified (mod) file that reduced load for feasibility in DC-OPF
+            forecasts_pointers_file=joinpath(
+                data_dir, "forecasts", "timeseries_pointers_da_7day_mod.json"
+            ),
+            add_reserves=df.Reserve[l],
+        )
+
+        # Add single generator at a defined bus
+        gen = add_gerator!(base_da_system, df.Offer_Bus[l], (min=0.0, max=0.0))
+        @test gen in get_components(Generator, base_da_system)
+
+        # create and set variable cost time-series for the generator
+        ts_array = create_generator_bids(;
+            initial_bidding_time=initial_bidding_time,
+            bidding_periods=df.bidding_period[l],
+            system=base_da_system,
+            costs=zeros(length(df.bidding_period[l])),
+        )
+        set_variable_cost!(base_da_system, gen, ts_array)
+
+        # duplicate system and prepare times series for the time varying parameters (loads, renewables, ...)
+        sys_uc, sys_ed = prep_systems_UCED(base_da_system)
+
+        # generic market formulation templates with defined network formulation
+        # CopperPlate-OPF: network=CopperPlatePowerModel
+        # DC-OPF: network=DCPPowerModel
+        # NFA-OPF (only line limit constraints): network=NFAPowerModel
+        # DC-PTDF-OPF (what ISOs do): network=StandardPTDFModel
+        template_uc = template_unit_commitment(; network=df.Network[l])
+        template_rt = template_economic_dispatch(; network=df.Network[l])
+        template_ed = template_economic_dispatch(; network=df.Network[l])
+
+        # build a market clearing simulator
+        market_simulator = UCEDRT(;
+            system_uc=sys_uc,
+            system_rt=sys_rt,
+            system_ed=sys_ed,
+            template_uc=template_uc,
+            template_rt=template_rt,
+            template_ed=template_ed,
+            solver_uc=solver_uc,
+            solver_rt=solver_rt,
+            solver_ed=solver_ed,
+        )
+
+        @test isa(market_simulator, UCEDRT)
+
+        directory_name= "Network_" * string(df.Network[l]) * "__Ramp_" * string(df.Ramp[l]) * 
+        "__Min_gen_" * string(df.Minimal_generation[l]) * "__Reserve_" * string(df.Reserve[l]) *
+        "__Offer_Bus_" * string(df.Offer_Bus[l]) * "__bidding_period_1-" * string(length(df.bidding_period[l]))
+
+        simulation_folder = joinpath(example_dir, "price_impact_analysis_5bus_nrel/results", directory_name)
+
+        lmps_df, results_df = load_pq_curves(market_simulator, range_quota, simulation_folder)
+
+        @test isa(results_df[range_quota[1]], Dict{String,SimulationResults})
+        @test isa(lmps_df[range_quota[1]], Dict{String,DataFrame})
+
+        for (y,t) in enumerate(period_analysed)
+            plt[x,y] = plot_price_curves(lmps_df, period_analysed[y], unique(df.Offer_Bus), df.Offer_Bus[l], initial_time)
+        end
+    end
+    return plt
+end
+
